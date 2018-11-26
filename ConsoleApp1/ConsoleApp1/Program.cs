ecd5f7b25f047d5954b9177516e735de60e881ce
Lab4 RUPPIN <lab4@ruppin.ac.il>
2018-11-26 12:22:06 +0200

    sssss

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack s = new Stack(3);
+            s.Push(1);
+            s.Push(2);
+            s.Push(3);
+            s.Push(4);
+            s.Push(4);
+            Console.WriteLine(s.Pop());
+            Console.WriteLine(s.Pop());
+            Console.WriteLine(s.Pop());
+            Console.WriteLine(s.Pop());
+            Console.WriteLine(s.Pop());
+
+            Console.WriteLine("____________________________");
+            s.Push(1);
+            s.Push(2);
+            s.Push(3);
+            s.Push(4);
+            s.Push(4);
+
+            while(s.Peek() != -1)
+            {
+                Console.WriteLine(s.Pop());
+            }
+        }
+    }
+
+    class Stack
+    {
+        int[] values;
+        int pointer;
+
+        public Stack() : this(10)
+        {
+            //values = new int[10];
+        }
+
+        public Stack(int size)
+        {
+            values = new int[size];
+            pointer = -1;
+        }
+
+        public void Push(int newValue)
+        {
+            if (pointer == values.Length - 1)
+            {
+                Console.WriteLine("Stack is full!!");
+            }
+            else
+            {
+                values[++pointer] = newValue;
+            }
+        }
+
+        public int Pop()
+        {
+            
+            if (pointer >= 0)
+            {
+                int ret = values[pointer];
+                values[pointer--] = 0;
+                return ret;
+            }
+            else
+            {
+                Console.WriteLine("pop() - Stack is empty!!");
+                return pointer;
+            }
+        }
+
+        public int Peek()
+        {
+            if(pointer<0)
+            {
+                Console.WriteLine("peek() - Stack is empty!");
+                return pointer;
+            }
+            return values[pointer];
+        }
+
+        public bool IsEmpty()
+        {
+            return pointer == -1;
+        }
+    }
+}
---
+++ VectorEX/VectorEX/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorEX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntegersVector blad = new IntegersVector();
+            blad.Add(5);
+            blad.Add(10);
+            blad.Add(1);
+            for(int i = 0;i<3;i++)
+            {
+                Console.WriteLine(blad.Get(i));
+            }
+        }
+    }
+    class IntegersVector
+    {
+        int[] integers;
+        int position;
+
+        public IntegersVector()
+        {
+            position = -1;
+            integers = new int[1];
+        }
+
+        public void Add(int newInteger)
+        {
+            if (integers.Length == position + 1)
+            {
+                integers[position] = newInteger;
+            }
+            
+        }
+
+        public int Get(int index)
+        {
+            return integers[index];
+        }
+
+        public bool IsEmpty()
+        {
+            return position == -1;
+        }
+
+        public int Size()
+        {
+            return position;
+        }
+
+        private void EnlargeVector()
+        {
+            int[] temp = new int[(position+1)*2];
+            integers.CopyTo(temp, 0);
+            integers = temp;
+        }
+    }
+}
